9cbc484d51652288df7a6051412aa1e9162da082
ruhsi <ruhsi@dev.com>
2026-05-03 12:42:02 +0530

    Initial commit

---
+++ fizz-buzz-kata/src/main/java/com/rm/fbk/FizzBuzzKataApplication.java
@@ -0,0 +1,13 @@
+package com.rm.fbk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FizzBuzzKataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FizzBuzzKataApplication.class, args);
+    }
+
+}
---
+++ fizz-buzz-kata/src/test/java/com/rm/fbk/FizzBuzzKataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rm.fbk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FizzBuzzKataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
