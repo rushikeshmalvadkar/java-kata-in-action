2040a4d58e4ccc6e762636b4432c09cb3b7ed3e4
rushi <rushi@dev.com>
2026-06-15 13:55:25 +0530

    initial comment

---
+++ log-analyzer-kata/src/main/java/com/rm/lak/LogAnalyzerKataApplication.java
@@ -0,0 +1,13 @@
+package com.rm.lak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogAnalyzerKataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogAnalyzerKataApplication.class, args);
+    }
+
+}
---
+++ log-analyzer-kata/src/test/java/com/rm/lak/LogAnalyzerKataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rm.lak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogAnalyzerKataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
